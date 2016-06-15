23ff72196fe42f7e7349a23a5d3a148b67104f85
Alex Egger <alex.egger96@gmail.com>
2016-06-15 20:22:58 +0200

    Add command handlers to Role interface

--- role.go
+++ role.go
@@ -7,6 +7,8 @@ type Role interface {
 	// Register registers with a Leader under the address leaderHost.
 	Register(leaderHost string) error
 
+	Unregister() error
+
 	// Handle handles all incoming messages.
 	Handle(m *Message, w io.Writer) error
 
@@ -18,4 +20,8 @@ type Role interface {
 	OnDelete(m *Message, w io.Writer) error
 	// OnNoop is called when an incoming message is of type Message_NOOP.
 	OnNoop(m *Message, w io.Writer) error
+	// OnGetAll is called when an incoming message is of type Message_GET_ALL.
+	OnGetAll(m *Message, w io.Writer) error
+	// OnExit is called when an incoming message is of type Message_EXIT.
+	OnExit(m *Message, w io.Writer) error
 }
